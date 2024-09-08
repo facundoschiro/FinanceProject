ae0367af96bb2f02f4c88af50edd13ceb960c1bb
Facundo Schiro <facundooscar2020@gmail.com>
2024-09-08 20:32:51 -0300

    Hono

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"])
+const isProtectedRoute = createRouteMatcher([
+  "/",
+  // "/api(.*)" Esto es por si quiero proteger la ruta de la api
+])
 
 export default clerkMiddleware((auth, request) => { 
   if (isProtectedRoute(request)) { 
@@ -14,8 +17,8 @@ export default clerkMiddleware((auth, request) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
