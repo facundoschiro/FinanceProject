3e53b984a78f92cac1d651b841a715245ed40288
Facundo Schiro <facundooscar2020@gmail.com>
2024-09-09 17:46:32 -0300

    Comienzo header

---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header className="bg-gradient-to-b from-blue-700 to-blue-500" px-4 py-8>
+        Header Component
+        </header>
+    ); 
+};
